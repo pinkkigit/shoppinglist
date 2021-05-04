35f3419c6f3a03c44aea6092eaa2b18fc039d709
pinkki <joonayv@gmail.com>
2021-05-04 20:59:51 +0300

    refactor user frontpage to own component

--- app/src/components/FrontPage.jsx
+++ app/src/components/FrontPage.jsx
@@ -1,56 +1,19 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Button, Header, Icon, List } from "semantic-ui-react";
 import "../index.css";
 import { v4 as uuid } from "uuid";
 import AuthStorageContext from "../contexts/AuthStorageContext";
-import userService from "../services/Users";
-import UserListItem from "./UserListItem";
+import UserFrontPage from "./UserFrontPage";
 
 const FrontPage = ({ handleLogOut }) => {
-  const [lists, setLists] = useState([]);
   const currentUser = useContext(AuthStorageContext);
-
-  useEffect(async () => {
-    if (currentUser.user) {
-      const user = await userService.getOne(currentUser.user.id);
-      setLists(user.lists);
-    }
-  }, [currentUser]);
 
   return (
     <>
       <Header as="h1">The best shopping list website ever</Header>
       {currentUser.user ? (
-        <>
-          <p>
-            Logged in as <strong>{currentUser.user.username}</strong>
-          </p>
-          <div>
-            <Link to={`/lists/${uuid()}`}>
-              <Button id="frontpage-buttons" compact>
-                <Icon name="write" />
-                Create a new list
-              </Button>
-            </Link>
-          </div>
-          {lists.length > 0 && (
-            <>
-              <Header as="h2">My lists</Header>
-              <List celled relaxed>
-                {lists.map((list) => (
-                  <UserListItem key={list.listId} list={list} />
-                ))}
-              </List>
-            </>
-          )}
-          <div>
-            <Button id="frontpage-buttons" compact onClick={handleLogOut}>
-              <Icon name="sign-out" />
-              Log out
-            </Button>
-          </div>
-        </>
+        <UserFrontPage handleLogOut={handleLogOut} />
       ) : (
         <>
           <div>
---
+++ app/src/components/UserFrontPage.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Header, Icon, List } from "semantic-ui-react";
+import "../index.css";
+import { v4 as uuid } from "uuid";
+import AuthStorageContext from "../contexts/AuthStorageContext";
+import userService from "../services/Users";
+import UserListItem from "./UserListItem";
+
+const UserFrontPage = ({ handleLogOut }) => {
+  const [lists, setLists] = useState([]);
+  const currentUser = useContext(AuthStorageContext);
+
+  useEffect(async () => {
+    if (currentUser.user) {
+      const user = await userService.getOne(currentUser.user.id);
+      setLists(user.lists);
+    }
+  }, [currentUser]);
+
+  return (
+    <>
+      <p>
+        Logged in as <strong>{currentUser.user.username}</strong>
+      </p>
+      <div>
+        <Link to={`/lists/${uuid()}`}>
+          <Button id="frontpage-buttons" compact>
+            <Icon name="write" />
+            Create a new list
+          </Button>
+        </Link>
+      </div>
+      {lists.length > 0 && (
+        <>
+          <Header as="h2">My lists</Header>
+          <List celled relaxed>
+            {lists.map((list) => (
+              <UserListItem key={list.listId} list={list} />
+            ))}
+          </List>
+        </>
+      )}
+      <div>
+        <Button id="frontpage-buttons" compact onClick={handleLogOut}>
+          <Icon name="sign-out" />
+          Log out
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default UserFrontPage;
